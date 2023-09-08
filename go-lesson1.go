4e9a6d50c762edb0e2e66d37095018e155e91fa4
furkan-kayapinar <johsquaree@gmail.com>
2023-09-08 18:05:55 +0300

    lesson1-finished

--- go-lesson1.go
+++ go-lesson1.go
@@ -1,14 +1,18 @@
 package main
 import "fmt"
+
+func tarih(x int,c int,v int){
+	fmt.Println(x,"/",c,"/",v)
+}
+func saat(s int,d int){
+	fmt.Println(s,":",d)
+}
 func main(){
-
-
-
-
-
-
-
-
-
-
+	s:=18
+	d:=35
+	x:=23
+	c:=4
+	v:=3214
+	tarih(x,c,v)
+	saat(s,d)
 }
