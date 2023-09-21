abb7272cda9bf3e41e7f4c41019f478172d9395a
furkan-kayapinar <johsquaree@gmail.com>
2023-09-21 19:23:20 +0300

    w-k-revision

--- weight-height/w-k.go
+++ weight-height/w-k.go
@@ -6,16 +6,30 @@ package main
 import "fmt"
 
 func main() {
-    var input float64
-    var w float64
+	var w,h float32
 
+	fmt.Print("VUCUT-KITLE ENDEXİNİZİ OGRENECESINIZ.\n")
+    //metre ve cm farkı olabilir.ben kullanıcıan cm ile veri aldım.
 
-    fmt.Println("Lütfen boy ve kilonuzu sirasiyla giriniz...")
-    fmt.Println("Boy Bilgisi:")
-    fmt.Scanf(&h)
-    fmt.Println("Kilo Bilgisi:")
-    fmt.Scanf(&w)
+	fmt.Print("boy katsayisini girin:\n(cm)")
+	fmt.Scan(&h)
+	fmt.Print("kilo katsayisini girin:\n(kilo) ")
+	fmt.Scan(&w)
 
     kutleEndexi := w / (h * h)
     fmt.Println("Vücut Kitle İndeksi:", kutleEndexi)
+    
+    if kutleEndexi< 0.0018500 {
+        fmt.Println("zayifsin yemek ye")
+    } else if kutleEndexi< 0.0024999 {
+        fmt.Println("normalsin ama abartma")
+    } else if kutleEndexi< 0.0029999{
+        fmt.Println("hafiften şişmansin dikkat et")
+    } else if kutleEndexi< 0.0034999 {
+        fmt.Println("şişmansin yemek yeme!")
+    } else if kutleEndexi<0.0040000{
+        fmt.Println("obezsin lütfen dur!")
+    } else {
+        fmt.Println("seni bu teste sokan şey ne?")
+    }
 }
